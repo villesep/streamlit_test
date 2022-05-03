cb6c0c3caeec95ef1dfbbcb971c68c5992273197
villesep <60987697+villesep@users.noreply.github.com>
2022-05-03 10:17:28 +0300

    Update streamlit_testfile.py

--- streamlit_testfile.py
+++ streamlit_testfile.py
@@ -32,7 +32,7 @@ st.bar_chart(hist_values)
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-write('Hello, *World!* :sunglasses:')
+st.write('Hello, *World!* :sunglasses:')
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
